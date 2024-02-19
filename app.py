b0c7e602021e6a6ea2080540c4a80dce18f803cc
Joe Vincent <hellojustjoe@gmail.com>
2024-02-19 15:12:54 -0600

    Linking git repo to local workspace

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas
+import numpy
+
+github_url = "https://github.com/Hellojustjoe/"
+badge_url = "https://img.shields.io/badge/-hellojustjoe-black?style=flat-square&logo=github"
+
+st.set_page_config(layout="wide")
+
+st.markdown(f"""
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <h1 style="margin-right: 10px;">Simple Data Science Dashboard</h1>
+            <a href="{github_url}">
+                <img src="{badge_url}" alt="GitHub Badge" style="height: 40px;">
+            </a>
+        </div>
+        """, unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;'>A basic dashboard using Streamlit and Pandas</h3>", unsafe_allow_html=True)
