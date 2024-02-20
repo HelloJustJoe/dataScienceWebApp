62991e1ba859ad7cb614772af5a62fed2469a6da
Joe Vincent <hellojustjoe@gmail.com>
2024-02-20 11:19:29 -0600

    Created sliders, multiselect, and have refined layout and presentation of dashboard. Will revist in future to add further functionality and predictive modelling.

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ st.markdown(f"""
         </div>
         """, unsafe_allow_html=True)
 
-st.markdown("<h4 style='text-align: center;'>Geospatial dashboard to visualize NYC Motor Vehicle Collisions - Built with Numpy, Pandas and Plotly.</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Geospatial dashboard visualizing NYC Motor Vehicle Collisions - Built with Numpy, Pandas and Plotly.</h4>", unsafe_allow_html=True)
+
+
 
 @st.cache_data(persist=True)
 def load_data(nrows):
@@ -32,43 +34,100 @@ def load_data(nrows):
     data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
     return data
 
-data = load_data(10000)
+data = load_data(100000)
 
-injured = data['injured_persons'].sum()
+injured = int(data['injured_persons'].sum())
 st.markdown(f'<h5 style="text-align: center;">Total number of people injured in NYC: {injured}</h5>', unsafe_allow_html=True)
+
+st.markdown('<hr style="border:1px solid #FFD700;"/>', unsafe_allow_html=True)
+
 
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("People injured in a collision:", 0, 19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
+filtered_data = data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any")
 
+initial_view_state = pdk.ViewState(
+    latitude=data['latitude'].median(),
+    longitude=data['longitude'].median(),
+    zoom=10,
+    pitch=0,
+)
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    filtered_data,
+    get_position='[longitude, latitude]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=100,
+)
+
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/dark-v9',
+    initial_view_state=initial_view_state,
+    layers=[layer],
+))
 
 st.header("How many collisions occur during a given time of day?")
 hour = st.slider('Hour:', 0, 23)
 data = data[data['date/time'].dt.hour == hour]
 
-st.markdown("Collisions between %i:00 and %i:00" % (hour, (hour+1) % 24))
+col1, col2, = st.columns(2)
+with col1:
+    st.subheader("By the Hour: %i:00 - %i:00" % (hour, (hour+1) % 24))
 
-st.write(pdk.Deck(
-    map_style="mapbox://styles/mapbox/dark-v9",
-    initial_view_state=pdk.ViewState(
-        latitude=data['latitude'].mean(),
-        longitude=data['longitude'].mean(),
-        zoom=10,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            'HexagonLayer',
-            data=data[['date/time', 'latitude', 'longitude']],
-            get_position=['longitude', 'latitude'],
-            radius=200,
-            elevation_scale=10,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-        )
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/dark-v9",
+        initial_view_state=pdk.ViewState(
+            latitude=data['latitude'].median(),
+            longitude=data['longitude'].median(),
+            zoom=10,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                'HexagonLayer',
+                data=data[['date/time', 'latitude', 'longitude']],
+                get_position=['longitude', 'latitude'],
+                radius=150,
+                elevation_scale=5,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            )
+        ]
+    ))
+
+with col2:
+    st.subheader("Minute by Minute: %i:00 - %i:00" % (hour, (hour+1) % 24))
+    filtered = data[
+        (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour + 1))
     ]
-))
+
+    hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
+    chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
+    fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.header("Top 5 dangerous streets by affected type")
+selected_types = st.multiselect(
+    'Affected Type',
+    ['Pedestrians', 'Cyclists', 'Motorists'],
+    ['Pedestrians', 'Cyclists', 'Motorists']
+)
+
+if len(selected_types) > 0:
+    query_conditions = " | ".join([f"injured_{affected_type.lower()} >= 1" for affected_type in selected_types])
+    filtered_data = data.query(query_conditions)
+
+    top_dangerous = filtered_data.nlargest(5, 'injured_persons')[["on_street_name",
+                                                                  "injured_pedestrians",
+                                                                  "injured_cyclists",
+                                                                  "injured_motorists"]]
+
+    st.write(top_dangerous)
+else:
+    st.write("Please select at least one affected type to display the top 5 dangerous streets.")
 
 
 if st.checkbox("Show Raw Data", False):
